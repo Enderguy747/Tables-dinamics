829e4ffccdb2096f2b4176ffda99a1088ac776fc
Enderguy747 <61768197+Enderguy747@users.noreply.github.com>
2021-12-11 23:16:15 -0600

    Add files via upload

---
+++ dinamic table/script.js
@@ -0,0 +1,62 @@
+
+
+$(document).ready(function () {
+
+	$('#tabla').DataTable({
+
+		responsive: true,
+
+		paging: false,
+
+		searching: true,
+
+		ordering: true,
+
+		language: {
+
+			"decimal": "",
+
+			"emptyTable": "No hay información",
+
+			"info": "Mostrando START a END de TOTAL Entradas",
+
+			//"infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+
+			"infoFiltered": "(Filtrado de MAX total entradas)",
+
+			"infoPostFix": "",
+
+			"thousands": ",",
+
+			"lengthMenu": "Mostrar MENU Entradas",
+
+			"loadingRecords": "Cargando...",
+
+			"processing": "Procesando...",
+
+			"search": "Buscar:",
+
+			"zeroRecords": "Sin resultados encontrados",
+
+			"paginate": {
+
+				"first": "Primero",
+
+				"last": "Ultimo",
+
+				"next": "Siguiente",
+
+				"previous": "Anterior"
+
+			}
+
+		},
+
+	});
+
+	$('.dataTables_length').addClass('bs-select');
+
+});
+
+
+
